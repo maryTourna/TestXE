1794df306d6fe508e43e06c350615585c3a14fa9
maryTourna <mary.tourna@gmail.com>
2024-12-17 11:10:01 +0200

    find number of images in each ad

--- src/test/java/tests/checkAds.java
+++ src/test/java/tests/checkAds.java
@@ -2,18 +2,24 @@ package tests;
 
 import base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 
 public class checkAds extends BaseTest {
     // Εντοπίζουμε όλα τα αποτελέσματα της σελίδας
     @Test
-    public void checkSearchRangeResults() {
+    public void checkSearchRangeResults() throws InterruptedException {
 
         // Καθορισμένα όρια
         int minPrice = 200;
@@ -22,37 +28,57 @@ public class checkAds extends BaseTest {
         int maxSize = 150;
 
 
-        //Περιμενουνε τα results
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-testid='property-ad-title']")));
-        // Εντοπίζουμε ΜΟΝΟ τις αγγελίες (εξαιρούμε τα widgets)
-        List<WebElement> propertyAds = driver.findElements(By.cssSelector("div[class='common-property-ad-title']"));
+        // Περιμένουμε να φορτωθούν τα αποτελέσματα
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.grid-x >div.lazyload-wrapper")));
+
+// Εντοπίζουμε ΜΟΝΟ τις αγγελίες (εξαιρούμε τα widgets)
+        List<WebElement> propertyAds = driver.findElements(By.cssSelector("div.grid-x >div.lazyload-wrapper.medium-4.scroll"));
 
 // Εκτυπώνουμε το πλήθος των αγγελιών που βρήκαμε
         System.out.println("Total property ads found: " + propertyAds.size());
 
+// Βρόχος για να μετρήσουμε τα images σε κάθε αγγελία
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        for (int i = 0; i < propertyAds.size(); i++) {
+            WebElement ad = propertyAds.get(i);
 
+            // Scroll στο συγκεκριμένο property ad για να ενεργοποιηθεί το lazy loading
+            js.executeScript("arguments[0].scrollIntoView(true);", ad);
+            Thread.sleep(1000); // Περίμενε για το lazy loading
 
-// Εξάγουμε πληροφορίες από κάθε αγγελία
-        for (WebElement ad : propertyAds) {
-            try {
-                // Παίρνουμε τον τίτλο της αγγελίας
-                String title = ad.findElement(By.xpath("//div[@class='common-property-ad-title']/h3[@data-testid='property-ad-title']")).getText();
-                int size = extractNumber(title); // Μετατροπή σε αριθμό
-                Assert.assertTrue(size >= minSize && size <= maxSize, "Size out of range: " + size);
+            // Εντοπίζουμε ΜΟΝΟ εικόνες μέσα σε img tag με div.slick-slide[style*='outline: none']
+            List<WebElement> images = propertyAds.get(i).findElements(By.cssSelector("div.slick-slide[style*='outline: none']"));
+            int countImages = images.size();
+            Assert.assertTrue(countImages <= 30, "number of images: " + countImages);
 
-                // Παίρνουμε την τιμή της αγγελίας
-                String price = ad.findElement(By.xpath("//span[@data-testid='property-ad-price']")).getText();
-                int priceInt = extractNumber(price); // Μετατροπή σε αριθμό
-                Assert.assertTrue(priceInt >= minPrice && priceInt <= maxPrice, "Price out of range: " + priceInt);
-
-                // Εκτύπωση για debugging
-                System.out.println("Verified ad: Price = " + price + ", Size = " + size);
-            }catch (Exception e) {
-                System.err.println("Failed to verify ad: " + e.getMessage());
-            }
-
+            //String propname =ad.getText();
+            // Εκτυπώνουμε τον αριθμό των εικόνων
+            //System.out.println(propname+"  "+"Property ad " + (i + 1) + " has " + images.size() + " images. //////////");
         }
-        System.out.println("All property ads have been verified successfully!");
+
+
+
+//// Εξάγουμε πληροφορίες από κάθε αγγελία
+//        for (WebElement ad : propertyAds) {
+//            try {
+//                // Παίρνουμε τον τίτλο της αγγελίας
+//                String title = ad.findElement(By.xpath("//div[@class='common-property-ad-title']/h3[@data-testid='property-ad-title']")).getText();
+//                int size = extractNumber(title); // Μετατροπή σε αριθμό
+//                Assert.assertTrue(size >= minSize && size <= maxSize, "Size out of range: " + size);
+//
+//                // Παίρνουμε την τιμή της αγγελίας
+//                String price = ad.findElement(By.xpath("//span[@data-testid='property-ad-price']")).getText();
+//                int priceInt = extractNumber(price); // Μετατροπή σε αριθμό
+//                Assert.assertTrue(priceInt >= minPrice && priceInt <= maxPrice, "Price out of range: " + priceInt);
+//
+//                // Εκτύπωση για debugging
+//                System.out.println("Verified ad: Price = " + price + ", Size = " + size);
+//            }catch (Exception e) {
+//                System.err.println("Failed to verify ad: " + e.getMessage());
+//            }
+
+       // }
+       // System.out.println("All property ads have been verified successfully!");
 
 
     }
